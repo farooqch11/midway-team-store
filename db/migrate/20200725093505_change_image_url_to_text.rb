4055376beeea8bfbc3ae397075bb3388ab790568
Junaid Hassan <junaidhassan2010@gmail.com>
2024-10-30 16:10:15 +0500

    migration updates

--- db/migrate/20200725093505_change_image_url_to_text.rb
+++ db/migrate/20200725093505_change_image_url_to_text.rb
@@ -1,9 +1,9 @@
 class ChangeImageUrlToText < ActiveRecord::Migration[5.2]
   def up
-    change_column :shopify_products, :image_url, :binary, :null => true
-    #Ex:- change_column("admin_users", "email", :string, :limit =>25)
+    change_column :shopify_products, :image_url, :string, null: true
   end
+
   def down
-    change_column :shopify_products, :image_url, :string, :null => true
+    change_column :shopify_products, :image_url, :string, null: true
   end
 end
