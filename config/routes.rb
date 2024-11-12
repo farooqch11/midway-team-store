73f043d0a2db606f9def21f8241c9aebe8dc6df1
Junaid Hassan <junaidhassan2010@gmail.com>
2024-11-13 01:25:39 +0500

    sidekiq jobs

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,9 @@
+require "sidekiq/web"
 Rails.application.routes.draw do
+  authenticate :user, ->(user) { user.admin? } do
+    mount Sidekiq::Web => '/sidekiq'
+  end
+
   root :to => "home#index"
   mount ShopifyApp::Engine, at: "/"
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
